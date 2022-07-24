0793ad6dfcc06ec9a1a3c3f64c9817befa9aaf70
kkureli <kkureli@magiclick.com>
2022-07-24 12:30:22 +0300

    AIP-004 feat: app navigation developed

---
+++ src/views/home/home.tsx
@@ -0,0 +1,12 @@
+import {TouchableOpacity, Text} from 'react-native';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <TouchableOpacity>
+      <Text>Home</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Home;
