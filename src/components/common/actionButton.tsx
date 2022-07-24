f5a487ea5ca78be680d3df8694d184d8baac7782
kkureli <kkureli@magiclick.com>
2022-07-24 21:01:45 +0300

    AIP-012 chore: action buttons changed pressable to touchable opacity

--- src/components/common/actionButton.tsx
+++ src/components/common/actionButton.tsx
@@ -1,4 +1,4 @@
-import {Pressable, StyleSheet} from 'react-native';
+import {StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
 import Typography from './typography';
 import {ActionButtonProps} from '../../utils/types/actionButton';
@@ -7,12 +7,12 @@ import Icon from './icon';
 const ActionButton = (props: ActionButtonProps) => {
   const {text, onPress, icon} = props;
   return (
-    <Pressable style={styles.container} onPress={onPress}>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       {icon && <Icon icon={icon} />}
       <Typography bold primary style={icon && styles.marginLeft5}>
         {text}
       </Typography>
-    </Pressable>
+    </TouchableOpacity>
   );
 };
 
